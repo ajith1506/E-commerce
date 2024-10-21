713a4707e5329780298b49745ac357bc39ec28af
ajith1506 <ajithkumararun1111@gmail.com>
2024-10-21 18:40:58 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ak15-e-commerce.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
